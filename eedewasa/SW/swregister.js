94b83667db5be8415334f86bd690778ce317bd9c
David Z <david23zack@gmail.com>
2019-02-05 13:35:32 +0700

    Update swregister.js

--- eedewasa/SW/swregister.js
+++ eedewasa/SW/swregister.js
@@ -6,7 +6,7 @@ if (navigator.serviceWorker.controller) {
 } else {
   //Register the ServiceWorker
   navigator.serviceWorker.register('pwabuilder-sw.js', {
-    scope: '/eedewasa/index.html'
+    scope: '/SW/'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
